d0c266be406d5c5a3ecd85c73cf6632fbd16d630
PamelaLimaa <layanepamela98@gmail.com>
2022-10-01 23:15:44 -0300

    modified:   dist/js/app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,5 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
 import { Negociacao } from './models/negociacao.js';
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
+const controller = new NegociacaoController();
